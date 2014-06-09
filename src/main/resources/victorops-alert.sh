d1cbba941176d310b4f687b318a999689ad8294e
kunalgupApDx <kunal.gupta@appdynamics.com>
2014-06-08 23:17:06 -0700

    fixing bug

--- src/main/resources/victorops-alert.sh
+++ src/main/resources/victorops-alert.sh
@@ -1,1 +1,1 @@
-..\..\..\jdk\bin\java -Dlog4j.configuration=file:conf/log4j.xml -jar victorops-alert.jar "$@"
+../../../jdk/bin/java -Dlog4j.configuration=file:conf/log4j.xml -jar victorops-alert.jar "$@"
